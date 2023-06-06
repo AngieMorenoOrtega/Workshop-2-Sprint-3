853c19e663f269bcb1cc523519fd96c2c5a46b60
angie moreno <guaiagaleria@gmail.com>
2023-06-06 11:45:40 -0500

    agregando context

--- src/Context/Context.js
+++ src/Context/Context.js
@@ -1,45 +1,23 @@
- import React, {createContext, useState} from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import { getPizzas } from '../Services/getpizzas';
 
-
-// export const CartContext = createContext();
-
-// export const ContextCarritoProvider = ({ children }) => {
-//     const [cartItems, setCartItems] = useState([]);
-
-//     const addToCart = (product) => {
-//         setCartItems([...cartItems, product]);
-//         console.log("cart items", cartItems)
-//     };
-
-//     const removeFromCart = (productId) => {
-//         const updatedCartItems = cartItems.filter(
-//             (item) => item.id !== productId
-//         );
-//         setCartItems(updatedCartItems);
-//     };
-
-//   return (
-//     <CartContext.Provider value={{cartItems, addToCart, removeFromCart}}>
-//         {children}
-//     </CartContext.Provider>
-//   )
-// }
 const PizzaContext = createContext();
-
-// Componente Proveedor del contexto
 const PizzaProvider = ({ children }) => {
-  // Aquí puedes inicializar y manejar los datos de las pizzas
-  const pizzas = [
-    {
-      id: 1,
-      nombre: "Pizza Margarita",
-      precio: 8.99,
-      reviews: 5,
-      descripcion: "Deliciosa pizza clásica con tomate, mozzarella y albahaca",
-      imagen: "margarita.jpg"
-    },
-    // ... otras pizzas ...
-  ];
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    const fetchPizzas = async () => {
+      try {
+        const pizzasData = await getPizzas();
+        setPizzas(pizzasData);
+      } catch (error) {
+        console.log(error);
+        setPizzas([]);
+      }
+    };
+
+    fetchPizzas();
+  }, []);
 
   return (
     <PizzaContext.Provider value={pizzas}>
@@ -48,7 +26,4 @@ const PizzaProvider = ({ children }) => {
   );
 };
 
-// Hook para acceder al contexto de las pizzas
-const usePizzaContext = () => useContext(PizzaContext);
-
-export { PizzaProvider, usePizzaContext };
+export { PizzaContext, PizzaProvider };
